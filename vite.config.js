0ff9deddcf0322b1eafd42e64e0d777be7b75da3
natopchane <natopchane@gmail.com>
2025-12-23 18:32:15 +0500

    Add Vite alias for assets to fix build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@assets': path.resolve(__dirname, './src/editorV2/assets'),
+    }
+  },
   build: {
     outDir: 'dist',
     sourcemap: false,
